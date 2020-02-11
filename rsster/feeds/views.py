4b2b9f3f4929a763f36cc1fbba355b4fa5ac3d4c
Appie verschoor <A.Verschoor@softwareprofs.nl>
2020-02-11 16:37:02 +0100

    Pagination the django way. Pretty easy I must say

--- rsster/feeds/views.py
+++ rsster/feeds/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.template import loader
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from .logic import harvest as harvest_one
 from feeds.models import Feed, Entry
 
@@ -7,7 +9,17 @@ from feeds.models import Feed, Entry
 def index(request):
     template = loader.get_template('feeds/index.html')
     latest_entries = Entry.entries.filter(feed__active=True).order_by('-published').select_related('feed')
-    context = {'latest_entries': latest_entries}
+    page = request.GET.get('page', 1)
+    pager = Paginator(latest_entries, 10)
+    try:
+        entries = pager.page(page)
+    except PageNotAnInteger:
+        entries = pager.page(1)
+    except EmptyPage:
+        entries = pager.page(paginator.num_pages)
+
+    context = {'latest_entries': entries}
+
     return HttpResponse(template.render(context, request))
 
 def feed_view(request, id=None):

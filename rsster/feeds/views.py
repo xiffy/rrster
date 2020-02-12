5fe444d9b892a41fbf0848b69dfb7141c0439ea1
Appie verschoor <A.Verschoor@softwareprofs.nl>
2020-02-12 09:26:57 +0100

    Fontje, ontdubbelen van de pager

--- rsster/feeds/views.py
+++ rsster/feeds/views.py
@@ -9,27 +9,18 @@ from feeds.models import Feed, Entry
 def index(request):
     template = loader.get_template('feeds/index.html')
     latest_entries = Entry.entries.filter(feed__active=True).order_by('-published').select_related('feed')
-    page = request.GET.get('page', 1)
-    pager = Paginator(latest_entries, 10)
-    try:
-        entries = pager.page(page)
-    except PageNotAnInteger:
-        entries = pager.page(1)
-    except EmptyPage:
-        entries = pager.page(paginator.num_pages)
-
+    entries = paged(latest_entries, request)
     context = {'latest_entries': entries}
-
     return HttpResponse(template.render(context, request))
 
 def feed_view(request, id=None):
     if not id:
         raise Http404("Please provide a FeedId")
-    else:
-        template = loader.get_template('feeds/index.html')
-        feed_entries = Entry.entries.filter(feed__id=id, feed__active=True)
-        context = {'latest_entries': feed_entries}
-        return HttpResponse(template.render(context, request))
+    template = loader.get_template('feeds/index.html')
+    feed_entries = Entry.entries.filter(feed__id=id, feed__active=True)
+    entries = paged(feed_entries, request)
+    context = {'latest_entries': entries}
+    return HttpResponse(template.render(context, request))
 
 
 def harvest(request):
@@ -37,3 +28,14 @@ def harvest(request):
     for site in candidates:
         harvest_one(site)
     return HttpResponse("Harvesting done!")
+
+def paged(entries, request):
+    page = request.GET.get('page', 1)
+    pager = Paginator(entries, 10)
+    try:
+        entries = pager.page(page)
+    except PageNotAnInteger:
+        entries = pager.page(1)
+    except EmptyPage:
+        entries = pager.page(pager.num_pages)
+    return entries

5b79667e548d545d88c454852d831e0853283e9f
xiffy <xiffy@xiffy.nl>
2020-02-21 14:22:24 +0100

    groepen introduceren, 1 op veel in de admin, groepen aan gebruikers koppelen ...

--- rsster/feeds/admin.py
+++ rsster/feeds/admin.py
@@ -2,15 +2,22 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-
 from .models import Feed, Entry, Group, GroupFeed
+
 
 class EntryAdmin(admin.ModelAdmin):
     readonly_fields = ['entry_created']
+
+
+class GroupFeedInline(admin.TabularInline):
+    model = GroupFeed
+
+
 class GroupAdmin(admin.ModelAdmin):
     list_display = ['description', 'id']
+    inlines = [GroupFeedInline]
 
 admin.site.register([Feed])
 admin.site.register(Entry, EntryAdmin)
 admin.site.register(Group, GroupAdmin)
-admin.site.register(GroupFeed)
+#admin.site.register(GroupFeed)
--- rsster/feeds/models/group.py
+++ rsster/feeds/models/group.py
@@ -7,4 +7,7 @@ class Group(models.Model):
     description = models.CharField(db_index=True, max_length=255)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "%s" % (self.description)
+
 
